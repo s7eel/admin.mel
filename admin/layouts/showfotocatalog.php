01655589174a03b4d40515f5bc675572af4d09fd
Marinailch <marina_ilch@mail.ru>
2017-01-05 17:00:58 +0300

    фронт добавления фото в каталог

--- admin/layouts/showfotocatalog.php
+++ admin/layouts/showfotocatalog.php
@@ -1,6 +1,7 @@
+
 <div class="header">
 	<div class="logo">
-		<a href="#"><img src="./images/logo1.png" width="130px">
+		<a href="admin/index.php"><img src="./images/logo1.png" width="130px">
 
 		</a>
 	</div>
@@ -9,26 +10,29 @@
 		<a href='#'><img src="./images/site.png" alt="Перейти на сайт"/></a>
 	</div>
 </div>
+<div class="add_foto">
+<form>
+	<div class="col-4">
+		<h4>Добавить фото в каталог</h4><br>
+		<input type='file' name="main_foto" accept="image/jpeg"><br>
+		<div>(фото должно быть не более 2МБ)</div>
+	</div>
+	<a href="index.php"
+</form>
+
+</div>
+<?php
+foreach ($arr_foto as $key=>$value): ?>
+	<div class="delete_foto">
+
+
+		<a href="<?= $_SERVER['PHP_SELF']?>?action=deletefotobyID&id=<?= $value['id']?>"><img src="../../assets/img/del.png" alt="Удалить"></a>
+		<img  width=250px  src="../../assets/img/projects/images/<?= $value['link']?>">
+
+	</div>
+
+<?php endforeach;?>
 <footer>
-	<?php
-	foreach ($arr_foto as $key=>$value): ?>
-		<div class="delete_foto">
-
-
-			<a href="<?= $_SERVER['PHP_SELF']?>?action=deletefotobyID&id=<?= $value['id']?>"><img src="../../assets/img/del.png" alt="Удалить"></a>
-			<img  width=250px  src="../../assets/img/projects/images/<?= $value['link']?>">
-
-		</div>
-
-	<?php endforeach;?>
-
-	<form>
-		<div class="col-4">
-			<h4>Добавить фото в каталог</h4><br>
-			<input type='file' name="main_foto" accept="image/jpeg"><br>
-			<div>(фото должно быть не более 2МБ)</div>
-		</div>
-	</form>
 
 
 </footer>
